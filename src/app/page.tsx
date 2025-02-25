09a345e148840efe9ae2cb1da54f78ed77bf6790
Joseph Stegall <steglljoseph09@gmail.com>
2025-02-25 16:30:45 -0500

    Forgot to save file before push.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-x1 bg-sky-500 p-6">
+    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-x1 bg-sky-500 p-6 shadow">
       <span>hi</span>
     </div>
   );

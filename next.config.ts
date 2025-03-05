d30363dfb7cbb5099fa477ba6b999e0cf580225b
jostegall <stegalljoseph09@gmail.com>
2025-03-05 00:36:05 -0500

    working on image paths for pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.NODE_ENV === 'production';
+const repoName = "web-portfolio"
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: isGitHubPages ? `/${repoName}` : "",
+  assetPrefix: isGitHubPages ? `/${repoName}` : "",
 };
 
 export default nextConfig;

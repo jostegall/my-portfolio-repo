25be3cc09dd7c89d5430bed79cfc604c63dce484
jostegall <stegalljoseph09@gmail.com>
2025-03-04 22:56:55 -0500

    updated about me section, finished styling website for the moment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           />
         </div>
         <div className="mt-2 sm:mx-2 sm:my-auto">
-          <span className="mx-auto sm:my-auto flex flex-row w-72 pb-4 justify-center border-b-2 text-3xl">Joseph Stegall</span>
+          <span className="mx-auto sm:my-auto flex flex-row w-72 pb-4 justify-center border-b-2 text-4xl">Joseph Stegall</span>
           <div className="mt-6 flex flex-row justify-center">
             <Link className="mx-1" href="https://www.linkedin.com/in/josteg/">
               <Image 
@@ -63,10 +63,27 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="p-6 bg-banner flex flex-col justify-center text-background rounded-b-xl shadow-xl">
-          <div className="sm:mx-20 grid grid-row-2 sm:grid-row-0 sm:grid-cols-2">
-            <span className="my-2 sm:mx-auto text-xl sm:w-6/12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas accusantium impedit illum modi nulla quos aliquam iusto ratione itaque eos quod ea rem asperiores suscipit cupiditate, facere molestiae adipisci dicta!</span>
-            <span className="my-2 sm:mx-auto text-xl sm:w-6/12">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas accusantium impedit illum modi nulla quos aliquam iusto ratione itaque eos quod ea rem asperiores suscipit cupiditate, facere molestiae adipisci dicta!</span>
+      {/* about section */}
+      <div className="hidden md:flex p-6 bg-banner text-background rounded-b-xl shadow-xl">
+          <div className="flex flex-col">
+            <span className="px-4 py-2 border-b-2 text-2xl sm:text-3xl">About Me</span>
+            <div className="flex flex-row">
+              <div className="mt-4 mx-20 p-6 w-6/12 border-l-2 border-gray-400 flex flex-col">
+                <span className="text-xl">
+                Hello! I'm Joseph - a senior Computer Science student with a passion for software development and problem-solving. Over the past few years, I've gained hands-on experience in coding, data strcutures and algorithms, and full-stack web development through various coursework and projects.
+                </span>
+                <span className="mt-4 text-xl">I thrive on tackling complex challenges and continuously expandinf my technical skill set. My interests lie in building efficient, scalable applications and exploring the intersection of technology and innovation.</span>
+                <span className="my-4 text-xl">Outside of coding, I enjoy reading various high fantasy and science fiction books, playing Dungeons & Dragons and Pathfinder, playing any title from FromSoftware, and hanging out with friends, my girlfriend, and my cat.</span>
+              </div>
+              <div className="mt-4 mx-auto content-center">
+                <Image className="border-2 border-background rounded-2xl shadow-xl"
+                  src="/images/smokey.jpeg"
+                  width={400}
+                  height={400}
+                  alt="picture of developer"
+                />
+              </div>
+            </div>
           </div>
       </div>
 
@@ -76,18 +93,15 @@ export default function Home() {
           
           {/* education card */}
           <div className="bg-card my-6 p-4 flex flex-col rounded-md shadow-md sm:text-lg">
-            <span className="mb-2 border-b-2 border-foreground font-bold text-center sm:text-left text-xl sm:text-2xl">Education</span>
+            <span className="border-b-2 border-foreground font-bold text-center sm:text-left text-xl sm:text-2xl">Education</span>
             <div className="mt-2 p-2 sm:hover:bg-sky-200 sm:hover:shadow-xl rounded-md">
-              <div className="mx-auto mb-2 sm:mx-4 grid grid-cols-2 font-bold">
+              <div className="mx-auto mb-2 sm:mx-4 grid grid-row-2 gap-1 sm:grid-row-0 sm:grid-cols-2 font-bold">
                 <span className="text-center sm:text-left">Indiana State University</span>
-                <span className="text-center sm:text-right">Expected: 12/2025</span>
+                <span className="text-center sm:text-right hidden sm:grid">Expected: 12/2025</span>
               </div>
-              <div className="mx-auto sm:mx-8 flex flex-col sm:px-4 sm:border-l-2 sm:border-foreground">
-              <span className="">
-                  BS in Computer Science
-                  <br />
-                  Security and Risk Minor
-                </span>
+              <div className="sm:mx-8 sm:px-4 sm:border-l-2 flex flex-col sm:border-foreground text-center sm:text-left max-w-5xl">
+                <span>BS in Computer Science, Concentrating in Information Sciences</span>
+                <span>Minor in Security and Risk </span>
               </div>
             </div>
           </div>
@@ -99,7 +113,7 @@ export default function Home() {
             
             {/* lead tech */}
             <div className="mt-2 p-2 sm:hover:bg-sky-200 sm:hover:shadow-xl rounded-md">
-              <div className="mx-auto mt-2 mb-2 sm:mx-4 grid grid-row-2 gap-1 sm:grid-row-0 sm:grid-cols-2 font-bold">
+              <div className="mx-auto mb-2 sm:mx-4 grid grid-row-2 gap-1 sm:grid-row-0 sm:grid-cols-2 font-bold">
                 <span className="text-center sm:text-left">Lead Building Technician & Programmer</span>
                 <span className="text-center sm:text-right hidden sm:grid">Hulman Memorial Student Union | ISU</span>
               </div>
@@ -114,7 +128,7 @@ export default function Home() {
 
             {/* norm tech */}
             <div className="mt-2 p-2 sm:hover:bg-sky-200 sm:hover:shadow-xl rounded-md">
-              <div className="mx-auto mt-2 mb-2 sm:mx-4 grid grid-row-2 gap-1 sm:grid-row-0 sm:grid-cols-2 font-bold">
+              <div className="mx-auto mb-2 sm:mx-4 grid grid-row-2 gap-1 sm:grid-row-0 sm:grid-cols-2 font-bold">
                 <span className="text-center sm:text-left">Building Technician & Programmer</span>
                 <span className="text-center sm:text-right hidden sm:grid">Hulman Memorial Student Union | ISU</span>
               </div>
